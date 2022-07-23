246b942ee271e94345992c3432f86d62a7dbeeb6
Denzel-Witbooi <dwitbooi28@gmail.com>
2022-07-23 18:41:53 +0200

    Add Saving the Cart to the Database
    All the changes necessary to save the shopping cart data to the db.

--- TRMDesktopUI/ViewModels/SalesViewModel.cs
+++ TRMDesktopUI/ViewModels/SalesViewModel.cs
@@ -15,12 +15,15 @@ namespace TRMDesktopUI.ViewModels
     {
         private IProductEndpoint _productEndpoint;
         private IConfigHelper _configHelper;
+        private ISaleEndpoint _saleEndpoint;
 
-        public SalesViewModel(IProductEndpoint productEndpoint, IConfigHelper configHelper)
+        public SalesViewModel(IProductEndpoint productEndpoint
+            , IConfigHelper configHelper, ISaleEndpoint saleEndpoint)
         {
             _productEndpoint = productEndpoint;
 
             _configHelper = configHelper;
+            _saleEndpoint = saleEndpoint;
            
         }
         protected override async void OnViewLoaded(object view)
@@ -186,7 +189,7 @@ namespace TRMDesktopUI.ViewModels
             NotifyOfPropertyChange(() => SubTotal);
             NotifyOfPropertyChange(() => Tax);
             NotifyOfPropertyChange(() => Total);
-
+            NotifyOfPropertyChange(() => CanCheckOut);
         }
 
 
@@ -206,6 +209,7 @@ namespace TRMDesktopUI.ViewModels
             NotifyOfPropertyChange(() => SubTotal);
             NotifyOfPropertyChange(() => Tax);
             NotifyOfPropertyChange(() => Total);
+            NotifyOfPropertyChange(() => CanCheckOut);
         }
 
         public bool CanCheckOut
@@ -214,14 +218,29 @@ namespace TRMDesktopUI.ViewModels
             {
                 bool output = false;
                 // Make sure there is something in the cart
+                if (Cart.Count > 0)
+                {
+                    output = true;
+                }
 
                 return output;
             }
         }
 
-        public void CheckOut()
+        public async Task CheckOut()
         {
+            // Create a SaleModel and post to the API
+            SaleModel sale = new SaleModel();
+            foreach (var item in Cart)
+            {
+                sale.SaleDetails.Add(new SaleDetailModel
+                {
+                    ProductId = item.Product.Id,
+                    Quantity = item.QuantityInCart
+                });
+            }
 
+           await _saleEndpoint.PostSale(sale);
         }
 
 
