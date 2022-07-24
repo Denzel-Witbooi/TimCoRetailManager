d87bfda08d8544b3030eb1fd67e2473670612ceb
Denzel-Witbooi <dwitbooi28@gmail.com>
2022-07-24 22:27:10 +0200

    Bug Fixes
    Fixes cart display bug, menu bugs, and more

--- TRMDesktopUI/ViewModels/ShellViewModel.cs
+++ TRMDesktopUI/ViewModels/ShellViewModel.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using TRMDesktopUI.EventModel;
+using TRMDesktopUI.Library.Models;
 
 namespace TRMDesktopUI.ViewModels
 {
@@ -12,22 +13,51 @@ namespace TRMDesktopUI.ViewModels
     {
         private IEventAggregator _events;
         private SalesViewModel _salesVM;
+        private ILoggedInUserModel _user;
+
         public ShellViewModel( 
             IEventAggregator events, 
-            SalesViewModel salesVM)
+            SalesViewModel salesVM, ILoggedInUserModel user)
         {
             _events = events;
             _salesVM = salesVM;
-
+            _user = user;
             // Represents the current instance of this class
             _events.Subscribe(this);
         
             ActivateItem(IoC.Get<LoginViewModel>());
         }
 
+        public bool IsLoggedIn
+        {
+            get
+            {
+                bool output = false;
+                if (string.IsNullOrWhiteSpace(_user.Token) == false)
+                {
+                    output = true;
+                }
+                return output;
+            }
+        }
+
+        public void ExitApplication()
+        {
+            TryClose(); 
+        }
+
+        public void LogOut()
+        {
+            _user.LogOffUser();
+            ActivateItem(IoC.Get<LoginViewModel>());
+            NotifyOfPropertyChange(() => IsLoggedIn);
+        }
+
+
         public void Handle(LogOnEvent message)
         {
             ActivateItem(_salesVM);
+            NotifyOfPropertyChange(() => IsLoggedIn);
         }
     }
 }
