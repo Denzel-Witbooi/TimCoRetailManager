318f2acf114c6454ac9dc03a76a64f768f179cde
Denzel-Witbooi <dwitbooi28@gmail.com>
2022-07-25 16:51:24 +0200

    Add Admin Endpoint Data
    Adds the for adminstrative endpoints.
    Does not yet secure the endpoints beyond authorization.

--- TRMDataManager.Library/DataAccess/SaleData.cs
+++ TRMDataManager.Library/DataAccess/SaleData.cs
@@ -84,14 +84,13 @@ namespace TRMDataManager.Library.DataAccess
           
         }
 
+        public List<SaleReportModel> GetSaleReport()
+        {
+            SqlDataAccess sql = new SqlDataAccess();
 
-        //public List<ProductModel> GetProducts()
-        //{
-        //    SqlDataAccess sql = new SqlDataAccess();
-
-        //    var output = sql.LoadData<ProductModel, dynamic>("dbo.spProduct_GetAll", new { }, "TRMData");
-
-        //    return output;
-        //}
+            var output = sql.LoadData<SaleReportModel, dynamic>("dbo.spSale_SaleReport", new { }, "TRMData");
+            
+            return output;
+        }
     }
 }
--- TRMDataManager/Controllers/SaleController.cs
+++ TRMDataManager/Controllers/SaleController.cs
@@ -21,5 +21,13 @@ namespace TRMDataManager.Controllers
 
         }
 
+        [Route("GetSalesReport")]
+        public List<SaleReportModel> GetSalesReport()
+        {
+            SaleData data = new SaleData();
+
+            return data.GetSaleReport();
+        }
+
     }
 }
